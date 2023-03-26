4df9c477ecfa150e2d59a76adeaab95d3e374a11
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-25 21:04:40 -1200

    assign the value the parameter

--- Program.cs
+++ Program.cs
@@ -2,11 +2,20 @@
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Welcome to the Test Maximum with Generics Problem");
+            Console.WriteLine("Please Enter the FirstValue:");
+            var val1 = Console.ReadLine();
+            Console.WriteLine("Please Enter the SecondValue:");
+            var val2 = Console.ReadLine();
+            Console.WriteLine("Please Enter the ThirdValue:");
+            var val3 = Console.ReadLine();
 
-            
+            var Value = FindMaxUsingGenerics.MaximumNum(val1, val2, val3);
+            Console.WriteLine("{0} is greater", Value);
+            Console.WriteLine("**************************************");
+
+
         }
     }
 }
