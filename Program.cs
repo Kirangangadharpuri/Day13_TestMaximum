3025c1471bdfa87bcd68cecd414a2ff76c98d941
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-25 21:19:49 -1200

    call print max method

--- Program.cs
+++ Program.cs
@@ -2,11 +2,25 @@
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Welcome to the Test Maximum with Generics Problem");
+            int[] valuesInt = { 1, 2, 3, 4, 5 };
+            double[] valuesDouble = { 10.4, 6.7, 8.2, 9.5, 7.3 };
+            string[] valuesString = { "ram", "shiv", "radha", "mohan" };
 
-            
+            FindMax<int> obj = new FindMax<int>(valuesInt);
+            obj.PrintMax();
+            Console.WriteLine("**********************************************");
+
+
+            FindMax<double> obj1 = new FindMax<double>(valuesDouble);
+            obj1.PrintMax();
+            Console.WriteLine("**********************************************");
+
+            FindMax<string> obj2 = new FindMax<string>(valuesString);
+            string MaxString = obj2.TestMaximum();
+            obj2.PrintMax();
+
         }
     }
 }
