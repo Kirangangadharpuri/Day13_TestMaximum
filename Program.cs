364898a044f77eea3607efff66ccba90563ba08f
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-25 20:51:30 -1200

    assign the value the parameter

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@
         {
             Console.WriteLine("Welcome to the Test Maximum with Generics Problem");
 
-            
+            Console.WriteLine("Please enter the first number");
+            string name1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter the second number");
+            string name2 = Console.ReadLine();
+
+            Console.WriteLine("Please enter the third number");
+            string name3 = Console.ReadLine();
+
+            string val = FindMaxString_UC3.MaximumFloatNumber(name1, name2, name3);
+            Console.WriteLine("{0} IS GREATER  VALUE.", val);
         }
     }
 }
